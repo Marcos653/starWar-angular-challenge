a1171e643c8bd9a8c458abee09589375b278c042
marcos-costa75047 <marcos.costa@perinity.com>
2024-07-02 22:10:23 -0300

    feat: challenge thrid day

--- src/app/service/movies.service.ts
+++ src/app/service/movies.service.ts
@@ -7,10 +7,11 @@ import { Movie, RespostaAPI } from './types/interface';
   providedIn: 'root'
 })
 export class MoviesService {
+  private apiUrl = 'https://swapi.dev/api/films';
 
   constructor(private httpClient: HttpClient) { }
 
   getMovies(): Observable<RespostaAPI<Movie>> {
-    return this.httpClient.get<RespostaAPI<Movie>>('/assets/films.json');
+    return this.httpClient.get<RespostaAPI<Movie>>(this.apiUrl);
   }
 }
